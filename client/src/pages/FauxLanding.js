2726079da3b33b19644e6d9e2f86080267406107
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-01 12:45:15 -0400

    Login

--- client/src/pages/FauxLanding.js
+++ client/src/pages/FauxLanding.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 const FauxLanding = () => {
 
     return (
-        <div>
-            <div>
-                <h1>Sign in to Twitter</h1>
-                <input type='text' value='Elonccentric'/>
-                <input type="password" id="pass" name="pass" value='KevinSucks'/>
-                <Link to='/home'><button>Log In</button></Link>
-            </div>  
+        <div className='landing'>
+            <div><img src='https://s7.gifyu.com/images/Screenshot-from-2022-04-01-12-42-25.png' /></div>
+            <h1>Sign in to Twitter</h1>
+            <ul>
+                <li><input type='text' value='Elonccentric'/></li>
+                <li><input type="password" id="pass" name="pass" value='KevinSucks'/></li>
+                <li><Link to='/home'><button>Log In</button></Link></li> 
+            </ul>  
         </div>
     )
 }
