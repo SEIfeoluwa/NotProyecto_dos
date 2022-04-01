e37994f5ab0da04c697edf40e397e586991afe43
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-01 04:50:25 -0400

    Home icon

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
      <div className="side-nav">
          <ul className="nav-links">
              <li className="item logo"><i class="fa-brands fa-twitter"></i></li>
-             <li className="item"><Link to="/home">Home</Link></li>
+             <li className="item"><i class="fa-solid fa-house"></i><Link to="/home">Home</Link></li>
              <li className="item"><i class="fa-solid fa-bell"></i><Link to="/IPP">Notifications</Link></li>
              <li className="item"><Link to="/IPP">Messages</Link></li>
              <li className="item"><Link to="/IPP">Bookmarks</Link></li>
