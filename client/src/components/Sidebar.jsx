bc057906a7379cc7e132bcafda82b3bf7fe2cf0d
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-01 04:45:58 -0400

    added logo

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,13 +4,14 @@ const Sidebar = () => {
     return (
      <div className="side-nav">
          <ul className="nav-links">
-             <li><Link to="/home">Home</Link></li>
-             <li><Link to="/IPP">Messages</Link></li>
-             <li><Link to="/IPP">Notifications</Link></li>
-             <li><Link to="/IPP">Bookmarks</Link></li>
-             <li><Link to="/IPP">Lists</Link></li>
-             <li><Link to="/prof">Profile</Link></li>
-             <li><Link to="/IPP">More</Link></li>
+             <li className="item logo"><i class="fa-brands fa-twitter"></i></li>
+             <li className="item"><Link to="/home">Home</Link></li>
+             <li className="item"><Link to="/IPP">Messages</Link></li>
+             <li className="item"><Link to="/IPP">Notifications</Link></li>
+             <li className="item"><Link to="/IPP">Bookmarks</Link></li>
+             <li className="item"><Link to="/IPP">Lists</Link></li>
+             <li className="item"><Link to="/prof">Profile</Link></li>
+             <li className="item"><Link to="/IPP">More</Link></li>
          </ul> 
     </div>   
     )
--- client/src/pages/IPPage.js
+++ client/src/pages/IPPage.js
@@ -1,8 +1,13 @@
+import Sidebar from "../components/Sidebar"
+
 const IPPage = () => {
     return (
         <div>
-            <h1>Page Under Construction</h1>
-            < img src="https://t3.ftcdn.net/jpg/02/24/86/92/360_F_224869243_WZHOVD3GqYq6ZUzRkZbQNTXzg1FzHwhd.jpg"/>
+            <Sidebar />  
+            <div>
+             <h1>Page Under Construction</h1>
+            < img src="https://t3.ftcdn.net/jpg/02/24/86/92/360_F_224869243_WZHOVD3GqYq6ZUzRkZbQNTXzg1FzHwhd.jpg"/>   
+            </div>
         </div>
     )
 }
